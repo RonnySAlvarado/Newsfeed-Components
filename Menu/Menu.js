af1ece34e132fb965b3f7d19812b5efdb8bfd45a
Ronny Alvarado <rsalvarado777@gmail.com>
2019-02-05 21:05:34 -0600

    added jquery via Microsoft CDN and animated the menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,14 +5,18 @@
   // console.log(menuButton);
 
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
-  console.log('pressed it');
-}
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+//   menu.classList.toggle('menu--open');
+//   console.log('pressed it');
+// }
+// menuButton.addEventListener('click', toggleMenu);
+
+$('.menu-button').click(function () { 
+  $('.menu').toggle("slide");
+});
+
 
 // Start Here: Create a reference to the ".menu" class
 // create a reference to the ".menu-button" class
 // Using your menuButton reference, add a click handler that calls toggleMenu
-
-menuButton.addEventListener('click', toggleMenu);
